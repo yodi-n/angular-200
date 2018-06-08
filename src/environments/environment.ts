b2c7cdc7ff6ef229bc533b1b60548c1e661a5fdf
nicolasyodi <yodi.n@sfeir.com>
2018-06-08 14:57:22 +0200

    step-18 env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    backend:    {
+        protocol:  'http',
+        host:      '127.0.0.1',
+        port:      '9000',
+        endpoints: {
+            allPeople:    '/api/peoples',
+            onePeople:    '/api/peoples/:id',
+            randomPeople: '/api/peoples/random'
+        }
+    }
 };
 
 /*
