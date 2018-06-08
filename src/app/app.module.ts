2f220270516737262a2c98d25db599e07026f090
nicolasyodi <yodi.n@sfeir.com>
2018-06-08 09:44:19 +0200

    step-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
+// CORE DEPS
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// MATERIAL DESIGN MODULES
+import {
+    MatToolbarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatIconModule,
+    MatListModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+
+
+// import { PeopleAppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatTabsModule,
+        MatButtonModule,
+        MatInputModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatIconModule,
+        MatListModule
+    ],
+    declarations: [
+        // PeopleAppComponent
+    ],
+    providers: [ ],
+    bootstrap: [
+        // PeopleAppComponent
+    ]
 })
 export class AppModule { }
